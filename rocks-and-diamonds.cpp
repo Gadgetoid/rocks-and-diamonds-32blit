36b5be51a7f5ae804de87c7d246ab02a6db316c6
Phil Howard <phil@gadgetoid.com>
2020-02-14 11:27:28 +0000

    Tidy up and give us a camera transform

--- rocks-and-diamonds.cpp
+++ rocks-and-diamonds.cpp
@@ -18,28 +18,40 @@ TileMap* level;
 
 Vec2 player = Vec2(12.0f, 1.0f);
 
-std::function<Mat3(uint8_t)> pan = [](uint8_t y) -> Mat3 {
-  Mat3 transform = Mat3::identity();
-  transform *= Mat3::translation(Vec2(player.x * 8.0f, player.y * 8.0f)); // offset to middle of world      
-  transform *= Mat3::translation(Vec2(-screen_width / 2, -screen_height / 2)); // transform to centre of framebuffer
-  return transform;
+Mat3 camera;
+
+// Line-interrupt callback for level->draw that applies our camera transformation
+// This can be expanded to add effects like camera shake, wavy dream-like stuff, all the fun!
+std::function<Mat3(uint8_t)> level_line_interrupt_callback = [](uint8_t y) -> Mat3 {
+  return camera;
 };
+
+void update_camera() {
+  // Create a camera transform that centers around the player's position
+  camera = Mat3::identity();
+  camera *= Mat3::translation(Vec2(player.x * 8.0f, player.y * 8.0f)); // offset to middle of world      
+  camera *= Mat3::translation(Vec2(-screen_width / 2, -screen_height / 2)); // transform to centre of framebuffer
+}
 
 void init() {
   set_screen_mode(ScreenMode::lores);
+
+  // Load the spritesheet from the linked binary blob
   screen.sprites = SpriteSheet::load((const uint8_t *)_binary_sprites_start);
+
+  // Load the level data from the linked binary blob
+  // Note: This will be const! Should probably copy the level data to a local array first
   level = new TileMap((uint8_t *)_binary_level_start, nullptr, Size(level_width, level_height), screen.sprites);
 }
 
 void render(uint32_t time_ms) {
-  screen.pen = Pen(20, 30, 40);
+  screen.pen = Pen(0, 0, 0);
   screen.clear();
 
-  screen.pen = Pen(80, 120, 160);
-  screen.text(data_sprites, minimal_font, Point(0, 0));
+  // Draw our level
+  level->draw(&screen, Rect(0, 0, screen.bounds.w, screen.bounds.h), level_line_interrupt_callback);
 
-
-  level->draw(&screen, Rect(0, 0, screen.bounds.w, screen.bounds.h), pan);
+  // Draw our character sprite
   screen.blit(screen.sprites, Rect(0, 24, 8, 8), Point(screen_width / 2, screen_height / 2), false);
 }
 
@@ -63,4 +75,6 @@ void update(uint32_t time) {
   }
 
   last_buttons = buttons;
+
+  update_camera();
 }
--- rocks-and-diamonds.hpp
+++ rocks-and-diamonds.hpp
@@ -11,5 +11,4 @@ void render(uint32_t time);
 extern const char _binary_level_start[];
 extern const char _binary_sprites_start[];
 
-const char *data_level = _binary_level_start;
-const char *data_sprites = _binary_sprites_start;
+void update_camera();
